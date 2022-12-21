21c2496482dd5327566ac84db09b7b94ce71010b
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-12-21 21:55:25 +0600

    get region from session

--- API/app.py
+++ API/app.py
@@ -5,21 +5,29 @@ purpose:
     * Generate Presigned URL
 """
 
-from flask import Flask, request, jsonify
+
 import boto3
 from botocore.config import Config
+from subprocess import Popen, PIPE
+from flask import Flask, request, jsonify
+
 
 # env specific vars
-REGION = 'ap-south-1'
 BUCKET = ''
+REGION = boto3.Session().region_name    # aws configure list | grep region | awk '{print $2}'
 
-# init
+
+# init flask
 app = Flask(__name__)
 
+# init aws client(s)
+s3 = boto3.client('s3', region_name=REGION, config=Config(signature_version='s3v4'))
+
 @app.route('/getpresignedurl', methods=["GET"])
-def testget():
+def generate_presigned_url():
     return jsonify({
-        "success" : True,
-        "httpStatusCode" : 200,
-        "sirgnedUrl" : "http://xyz.com"
+        "success"           : True,
+        "httpStatusCode"    : 200,
+        "sirgnedUrl"        : "http://xyz.com",
+        "aws_region"        : REGION
     })
